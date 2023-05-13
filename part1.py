15ef51bfa99b462644a81c6252e4b2627bb1e930
Ranay <kajyrlaranaj.com>
2023-05-13 15:28:20 +0600

    fourth  commit

--- part1.py
+++ part1.py
@@ -11,6 +11,4 @@ loc1= driver.find_element(By.CLASS_NAME,'gLFyf')
 loc1.send_keys("Selenium")
 loc1.send_keys(Keys.ENTER)
 time.sleep(10)
-loc2= driver.find_element(By.XPATH,"/html/body/div[6]/div/div[11]/div[1]/div[2]/div[2]/div/div/div[1]/div/div/div/div/div/div/div/div[1]/a/h3").click()
-time.sleep(10)
 
